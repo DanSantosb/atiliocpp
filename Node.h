1f10ab1bc76df3d66dc420e9fa1f7731f58d2ee9
Jotave-xe <joaosousavictor100@gmail.com>
2025-01-24 19:13:50 -0300

    Update Node.h

--- Node.h
+++ Node.h
@@ -12,7 +12,7 @@ struct Node {
     // construtor do node
     Node( double valor, Node* direta = nullptr, Node* abaixo = nullptr, int linha = 0, int coluna = 0) {
         double valor;
-        this->direita = direita;
+        this-> direita = direita;
         this-> abaixo = abaixo;
 
     }
