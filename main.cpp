f0abb5da32b8d838adea0187a5b03696535dadc3
DanSantosb <danilosantos.contato27@gmail.com>
2025-01-28 21:14:20 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,61 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include "Sparsematrix.h"
+#include "Node.h"
+
 using namespace std;
-
-int main() {
-    int linhas, colunas;
-    cin >> linhas >> colunas;
-
-    SparseMatrix matriz(linhas, colunas);
-
-    for (int i = 1; i <= linhas; ++i) {
-        for (int j = 1; j <= colunas; ++j) {
-            double valor;
-            cin >> valor;
-            matriz.insert(i, j, valor);
-        }
+// Função para ler uma matriz esparsa de um arquivo
+void readSparseMatrix(SparseMatrix& m, const string& matrizA) {
+    ifstream arquivo(matrizA);
+    
+    if (!arquivo.is_open()) { //verifica se o arquiuvo foi aberto corretamente 
+        cout << "Erro ao abrir o arquivo"<<endl;
+        return;
     }
 
-    cout << "Matriz inserida:\n";
-    matriz.print();
+    //bloco que verifica o formato da matrix
+    int linhas, colunas;
+    arquivo >> linhas >> colunas;
+    m = SparseMatrix(linhas, colunas);
+
+    //bloco de posição e valor dos produtos da matrix
+    int i, j;
+    double valor;
+    while (arquivo >> i >> j >> valor) {
+        m.inserir(i, j, valor);
+    }
+
+    arquivo.close();
+
+    
+}
+
+
+int main() {
+    SparseMatrix A, B;
+    fstream arquivo;
+    string horizonte;
+
+    readSparseMatrix(A, "matrizA.txt");
+    readSparseMatrix(B, "matrizB.txt");
+
+    arquivo.open("matrizA.txt", fstream::in);
+    if (arquivo.is_open()){
+        while (getline(arquivo,horizonte)){//le a linha do arquivo e add na variavel linha
+            cout<<horizonte<<endl;
+        }
+    }else{
+        cout<<"erro"<<endl;
+    }
+    arquivo.close();
+
+    cout << "Matriz A:" << endl;
+    A.print();
+
+    //cout << "Matriz B:" << endl;
+    //B.print();
 
     return 0;
 }
+
