b881d27796bd174af40bd90fd1bf4473a77f0872
Jotave-xe <joaosousavictor100@gmail.com>
2025-01-30 19:59:36 -0300

    Update Sparsematrix.h

--- Sparsematrix.h
+++ Sparsematrix.h
@@ -3,62 +3,86 @@
 
 #include <iostream>
 #include "Node.h"
+using namespace std;
 
 class SparseMatrix {
 private:
-    Node* head_linhas; // ponteiro para o sentinela das linhas
-    Node* head_colunas; // ponteiro para o sentinela das colunas
-    int linhas;
-    int colunas;
+    Node* head_linhas;  // nó sentinela das linhas
+    Node* head_colunas; // nó sentinela das colunas
+    int linhas;         // linhas da matriz
+    int colunas;        // colunas da matriz
 
 public:
-    SparseMatrix(int l, int c) : linhas(l), colunas(c) {
+    //recebe o numero de linhas e colunas, e analisa se os valores são validos
+    SparseMatrix(int l, int c) {
         if (l <= 0 || c <= 0) {
-            throw std::invalid_argument("ERRO: Valores de linha e(ou) coluna invalidos, tente novamente.");
+            throw invalid_argument("ERRO: Valores de linha e(ou) coluna invalidos, tente novamente.");
         }
 
+        linhas = l;
+        colunas = c;
+
+        // cria os sentinelas para as linhas e colunas
         head_linhas = new Node(0, nullptr, nullptr, 0, 0);
         head_colunas = new Node(0, nullptr, nullptr, 0, 0);
 
         Node* atual = head_linhas;
-        for (int i = 1; i <= l; i++) {
-            Node* novo = new Node(0, nullptr, nullptr, i, 0);
-            atual->abaixo = novo;
-            novo->direita = novo;
+        for(int i = 1; i <= l; i++){
+            Node* novo = new Node(0, nullptr, nullptr, i, 0); //cria um novo sentinela para a linha
+            atual -> abaixo = novo; // conecta os sentinelas das linhas
+            novo -> direita = novo;
             atual = novo;
         }
-        atual->abaixo = head_linhas;
+        atual -> abaixo = head_linhas; // tornacircular
 
-        atual = head_colunas;
-        for (int i = 1; i <= c; i++) {
-            Node* novo = new Node(0, nullptr, nullptr, 0, i);
-            atual->direita = novo;
-            novo->abaixo = novo;
+        atual = head_colunas; //axiliar para conectar os sentinelas
+        for(int i = 1; i <= c; i++){
+            Node* novo = new Node(0, nullptr, nullptr, 0, i); //cria um novo sentinela para a coluna
+            atual -> direita = novo; // conecta os sentinelas das colunas
+            novo -> direita = novo;
             atual = novo;
         }
-        atual->direita = head_colunas;
+        atual -> direita = head_colunas;// torna circular
     }
 
+    // Destrutor
     ~SparseMatrix() {
         desalocar();
     }
 
-    void desalocar() {
-        Node* linha_atual = head_linhas->abaixo;
-        while (linha_atual != head_linhas) {
-            Node* atual = linha_atual;
-            linha_atual = linha_atual->abaixo;
+    void print() {
+        Node* atualLinha = head_linhas->abaixo;
+        while (atualLinha != head_linhas) { // Andar pelas linhas
+            Node* atual = atualLinha->direita;
 
-            Node* coluna_atual = atual->direita;
+            for (int j = 1; j <= colunas; j++) {   // Andar pelas colunas
+                if (atual != atualLinha && atual->coluna == j) {
+                    cout << atual->valor << " ";
+                    atual = atual->direita;
+                } else {
+                    cout << "0 ";
+                }
+            }
+            cout << endl;
+            atualLinha = atualLinha->abaixo;
+        }
+    }
+
+    void desalocar() {
+        Node* linha_atual = head_linhas -> abaixo;
+        while (linha_atual != head_linhas) { //percorre a matriz
+            Node* atual = linha_atual;
+            linha_atual = linha_atual -> abaixo;
+
+            Node* coluna_atual = atual -> direita;
             while (coluna_atual != atual) {
                 Node* aux = coluna_atual;
-                coluna_atual = coluna_atual->direita;
+                coluna_atual = coluna_atual -> direita;
                 delete aux;
             }
-
             delete atual;
         }
-
+        // libera os sentinelas
         delete head_linhas;
         delete head_colunas;
         head_linhas = nullptr;
@@ -66,81 +90,63 @@ public:
     }
 
     void inserir(int l, int c, double valor) {
-        if (l <= 0 || l > linhas || c <= 0 || c > colunas) {
-            throw std::invalid_argument("ERRO: Valores de linha e(ou) coluna invalidos, tente novamente.");
-        }
-        if (valor == 0) {
+        if (l > linhas || c > colunas || l <= 0 || c <= 0) {
+            throw invalid_argument("ERRO: Valores de linha e(ou) coluna invalidos, tente novamente.");
+        }else if (valor == 0) {
             return;
         }
-
+        //procura a linha certa
+        Node* atualLinha = head_linhas;
+        while (atualLinha -> linha != l) {
+            atualLinha = atualLinha->abaixo;
+        }
+        // encontra a posição correta na linha
+        Node* prevLinha = atualLinha;
+        while (prevLinha -> direita != atualLinha && prevLinha -> direita -> coluna < c) {
+            prevLinha = prevLinha->direita;
+        }
+        // procura a coluna certa
+        Node* atualColuna = head_colunas;
+        while (atualColuna -> coluna != c) {
+            atualColuna = atualColuna->direita;
+        }
+        // encontra a posição correta na coluna
+        Node* prevColuna = atualColuna;
+        while (prevColuna -> abaixo != atualColuna && prevColuna -> abaixo -> linha < l) {
+            prevColuna = prevColuna->abaixo;
+        }
+        // cria o novo nó
         Node* novo = new Node(valor, nullptr, nullptr, l, c);
-
-        // Inserir na linha
-        Node* linha_atual = head_linhas;
-        while (linha_atual->linha != l) {
-            linha_atual = linha_atual->abaixo;
-        }
-
-        Node* coluna_atual = linha_atual;
-        while (coluna_atual->direita != linha_atual && coluna_atual->direita->coluna < c) {
-            coluna_atual = coluna_atual->direita;
-        }
-        novo->direita = coluna_atual->direita;
-        coluna_atual->direita = novo;
-
-        // Inserir na coluna
-        Node* coluna_sent = head_colunas;
-        while (coluna_sent->coluna != c) {
-            coluna_sent = coluna_sent->direita;
-        }
-
-        Node* linha_coluna = coluna_sent;
-        while (linha_coluna->abaixo != coluna_sent && linha_coluna->abaixo->linha < l) {
-            linha_coluna = linha_coluna->abaixo;
-        }
-        novo->abaixo = linha_coluna->abaixo;
-        linha_coluna->abaixo = novo;
+        novo -> direita = prevLinha->direita;
+        novo -> abaixo = prevColuna->abaixo;
+        prevLinha->direita = novo;
+        prevColuna->abaixo = novo;
     }
 
     double devolver(int l, int c) {
         if (l <= 0 || l > linhas || c <= 0 || c > colunas) {
-            throw std::invalid_argument("ERRO: Valores de linha e(ou) coluna invalidos, tente novamente.");
+            throw invalid_argument("ERRO: Valores de linha e(ou) coluna invalidos, tente novamente.");
+        }else{
+        // procura a linha certa
+        Node* atualLinha = head_linhas;
+        while (atualLinha->linha != l) {
+            atualLinha = atualLinha->abaixo;
         }
-
-        Node* linha_atual = head_linhas;
-        while (linha_atual->linha != l) {
-            linha_atual = linha_atual->abaixo;
-        }
-
-        Node* coluna_atual = linha_atual->direita;
-        while (coluna_atual != linha_atual) {
-            if (coluna_atual->coluna == c) {
-                return coluna_atual->valor;
+        // procura a  coluna certa
+        Node* atual = atualLinha->direita;
+        while (atual != atualLinha) {
+            if (atual->coluna == c) {
+                return atual -> valor;
             }
-            coluna_atual = coluna_atual->direita;
+            atual = atual -> direita;
         }
 
+        // se não encontrar retorna 0
         return 0;
-    }
-
-    void print() {
-        Node* linha_atual = head_linhas->abaixo;
-        while (linha_atual != head_linhas) {
-            Node* coluna_atual = linha_atual->direita;
-
-            for (int j = 1; j <= colunas; j++) {
-                if (coluna_atual != linha_atual && coluna_atual->coluna == j) {
-                    std::cout << coluna_atual->valor << " ";
-                    coluna_atual = coluna_atual->direita;
-                } else {
-                    std::cout << "0 ";
-                }
-            }
-
-            std::cout << std::endl;
-            linha_atual = linha_atual->abaixo;
         }
     }
+
+    
 };
 
-#endif // SPARSE_MATRIX_H
+#endif
